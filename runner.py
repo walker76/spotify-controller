da5ec58b98572ca6a0ca076fd8e22b620fb806e6
Andrew Walker <Andrew_Walker2@baylor.edu>
2019-04-24 16:13:51 -0500

    Small updates

--- runner.py
+++ runner.py
@@ -1,8 +1,14 @@
 import sys
+import os
 from client import Spotify
 import spotipy.util as util
 from auth_values import AuthValues
 from scope_builder import ScopeBuilder
+from sqlitedict import SqliteDict
+
+store = SqliteDict('./store.sqlite', autocommit=True)
+
+store['test_id'] = "spotify:user:andrew_walker2:playlist:6VXKlqxCX4ItIHWgFT9I6c"
 
 scope = ScopeBuilder().library().spotify_connect().get_scopes()
 
@@ -23,7 +29,14 @@ if token:
     print(sp.current_user())
     print(sp.devices())
     devices = sp.devices()
-    sp.play_track("spotify:user:andrew_walker2:playlist:6VXKlqxCX4ItIHWgFT9I6c", devices['devices'][0]['id'])
+
+    sp.play_track(store['test_id'], devices['devices'][0]['id'])
+
+    if store.__contains__("test_id"):
+        url = input("Please enter a Spotify URL: ")
+        print(url)
 
 else:
     print("Can't get token for", username)
+
+store.close()
